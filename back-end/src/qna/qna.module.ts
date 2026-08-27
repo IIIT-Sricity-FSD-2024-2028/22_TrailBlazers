addd72d6d505c55efe67274c83dfe022d5f3385e
sidduvanam07 <sidduvanam07@gmail.com>
2026-08-27 12:08:27 +0530

    Delete back-end directory

--- back-end/src/qna/qna.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { QnaController } from './qna.controller';
-import { QnaService } from './qna.service';
-
-@Module({
-  controllers: [QnaController],
-  providers: [QnaService],
-})
-export class QnaModule {}
